9f348588276d90c9a860d5cb8eb31fea56d780e8
Gerson <gersoncastrogomes95@gmail.com>
2024-12-05 02:35:36 -0300

    Refatorando o código e adicionando um footer

--- praTiForum/src/App.jsx
+++ praTiForum/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
-import './App.css'
+import Footer from "./layouts/footer"
+import './assets/styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+
+      <Footer />
     </>
   )
 }
---
+++ praTiForum/src/layouts/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../assets/styles/footer.css"
+
+const Footer = () =>{
+
+    return(
+        <footer className="footer">
+            <div className="footer-content">
+                <p>+PraTi Fórum 2024 | Todos os direitos reservados</p>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
