5949aa2877beb6f7558d7832021a6f5fa3f7a3df
Karine Pogozeleski <l.eduardo_rodrigues@hotmail.com>
2025-01-27 15:52:08 -0300

    App

--- praTiForum/src/App.jsx
+++ praTiForum/src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
-import Footer from "./layouts/footer"
-import './assets/styles/App.css'
+// import { useState } from 'react'
+// import Footer from './Atom/footer/Footer'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+
+//       <Footer/>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import Cadastro from "./pages/SignPage/SignPage";
+import Logado from "./pages/Logado/Logado";
+import Profile from "./pages/Profile/Profile"; // gabi
+import About from "./pages/About/About";
+import Institutional from "./pages/Institutional/Institutional";
+
+
 
 function App() {
   return (
-    <>
-
-      <Footer />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Página inicial */}
+        <Route path="/team" element={<About />} /> {/* Página Sobre */}
+        <Route path="/institutional" element={<Institutional />} /> {/* Página sobre a Instituição */}
+        <Route path="/login" element={<LoginPage />} /> {/* Página de Login */}
+        <Route path="/cadastro" element={<Cadastro />} /> {/* Página de Cadastro */}
+        <Route path="/logado" element={<Logado />} /> {/* Página logado */}
+        <Route path="/perfil-usuario" element={<Profile />} /> {/* Página de usuário */} 
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
