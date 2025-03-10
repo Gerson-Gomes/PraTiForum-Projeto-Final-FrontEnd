e20774b701b2934a3a0a655f662dc46cea61b5a2
Gerson Gomes <119882973+Gerson-Gomes@users.noreply.github.com>
2025-03-10 20:29:36 -0300

    Revert "ajustes de responsividade na tela de posts"

--- praTiForum/src/Molecule/Header/HeaderLogado/HeaderLogado.jsx
+++ praTiForum/src/Molecule/Header/HeaderLogado/HeaderLogado.jsx
@@ -2,20 +2,17 @@ import "./HeaderLogado.css";
 import InputSearch from "../../../Atom/input/InputSearch";
 import Logo from "../../../Atom/Logo/Logo";
 import ProfilePicture from "../../../Atom/perfil/ProfilePicture";
-import MenuMobile from "../../MenuMobile/MenuMobile";
 import profileDefault from "../../../Atom/icons/icon-profile.png"
 import { useNavigate } from "react-router-dom";
-
 
 
 function HeaderLogado() {
   const navigate = useNavigate()
   return (
     <nav className="headerLogado">
-      <MenuMobile showProfileIcon={true} />
       <Logo className="headerLogo" />
-      <div>
-        <InputSearch label="" tipo="text" placeholder="Pesquisar" className="search" />
+      <div >
+        <InputSearch label="" tipo="text" placeholder="Pesquisar" className="" />
       </div>
       <div className="header-perfil">
         <button id="perfilButton" onClick={() => navigate("/perfil-usuario${userId}")}>
